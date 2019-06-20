84ac5f525f35667cf49d10ea6e1320f8cc9dca0c
joao <joaomarcus13@hotmail.com>
2019-06-20 14:09:27 -0300

    criada classe Emprestimo joaoamrcos#3

--- src/main/Emprestimo.java
+++ src/main/Emprestimo.java
@@ -1,5 +1,55 @@
 package main;
+import java.util.Date;
 
 public class Emprestimo {
 
+	private Exemplar exemplar;
+	private Locatario locatario;
+	private Date data_emp;
+	private Date data_dev;
+	
+	public Emprestimo(Exemplar exemplar, Locatario locatario, Date data_emp, Date data_dev) {
+		super();
+		this.exemplar = exemplar;
+		this.locatario = locatario;
+		this.data_emp = data_emp;
+		this.data_dev = data_dev;
+	}
+
+	public Exemplar getExemplar() {
+		return exemplar;
+	}
+
+	public void setExemplar(Exemplar exemplar) {
+		this.exemplar = exemplar;
+	}
+
+	public Locatario getLocatario() {
+		return locatario;
+	}
+
+	public void setLocatario(Locatario locatario) {
+		this.locatario = locatario;
+	}
+
+	public Date getData_emp() {
+		return data_emp;
+	}
+
+	public void setData_emp(Date data_emp) {
+		this.data_emp = data_emp;
+	}
+
+	public Date getData_dev() {
+		return data_dev;
+	}
+
+	public void setData_dev(Date data_dev) {
+		this.data_dev = data_dev;
+	}
+	
+	
+	
+	
+	
 }
